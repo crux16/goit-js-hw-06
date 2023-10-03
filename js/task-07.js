123f2058f3b8b8118e7329c1b4d74a8940a2f68d
Rowell Calipay <30309887+crux16@users.noreply.github.com>
2023-10-03 18:23:31 +0800

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,14 @@
 const sizeController = document.querySelector('#font-size-control');
 const output = document.querySelector('#text');
 
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215)
+     .toString(16)
+     .padStart(6, 0)}`;
+ }
+
 sizeController.addEventListener('input', () => {
    output.style.fontSize = `${sizeController.value}px`;
+   output.style.color = getRandomHexColor();
+   output.style.transition = 'font-size 250ms ease,color 250ms ease'
 });
