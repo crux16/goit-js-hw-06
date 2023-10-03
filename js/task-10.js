e42cd828a9e7b1e2d5a41a6a0381d9e79c77bdab
Rowell Calipay <30309887+crux16@users.noreply.github.com>
2023-10-03 13:39:10 +0800

    task 10 added

--- js/task-10.js
+++ js/task-10.js
@@ -6,11 +6,42 @@ function getRandomHexColor() {
 
 const createBtn = document.querySelector('#controls button[data-create]');
 const destroyBtn = document.querySelector('#controls button[data-destroy]');
-const inputRange = document.querySelector('#constrols input');
+const inputRange = document.querySelector('#controls input[type="number"]');
 const boxes = document.querySelector('#boxes');
+let inputAmount = 0;
 
-
-
-createBtn.addEventListener('click', () =>{
+function createBoxes(amount) {
   
-})
+  if(amount){
+    let boxSize = 30;
+    for(let i = 0; i < amount; i += 1){
+      const box = document.createElement('div');
+      box.className = 'box';
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${boxSize}px`;
+      box.style.height = `${boxSize}px`;
+      box.style.border = `1px inset ${getRandomHexColor()}`;
+      boxes.appendChild(box);
+
+      boxSize += 10;
+    }
+    return;
+  }
+   return alert('no amount inputted!');
+}
+
+inputRange.addEventListener('input', (event) =>{
+  if (event.currentTarget.value > 100){
+    alert('100 is the max range!');
+    event.currentTarget.value = 100;
+  }
+  inputAmount = event.currentTarget.value;
+});
+
+createBtn.addEventListener('click', () => {
+  return createBoxes(inputAmount);  
+});
+
+destroyBtn.addEventListener('click', () => {
+  return boxes.innerHTML = '';
+});
